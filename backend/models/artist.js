f0b9fe84ec2352a6649905196eeb53be816b4509
Joshua Gato <joshuagato37@gmail.com>
2025-12-17 16:53:52 +0000

    File reformatted

--- backend/models/artist.js
+++ backend/models/artist.js
@@ -1,17 +1,21 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/sequelizedb');
+const Sequelize = require("sequelize");
+const sequelize = require("../util/sequelizedb");
 
-const Artist = sequelize.define('artist', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  }
-}, { timestamps: false });
+const Artist = sequelize.define(
+    "artist",
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+    },
+    { timestamps: false }
+);
 
 module.exports = Artist;
