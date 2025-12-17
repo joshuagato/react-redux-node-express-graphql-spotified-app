c4b104131f49f58f207415887cec29f97629fb79
Joshua Gato <joshuagato37@gmail.com>
2025-12-17 17:40:14 +0000

    Added music home component

---
+++ frontend/src/components/MusicHome/UpperSection/UpdateDetails/Input/Input.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Input.scss";
+
+const Input = (props) => (
+    <input
+        className="update-input"
+        name={props.name}
+        type={props.type}
+        placeholder={props.pholder}
+        onChange={props.changed}
+        value={props.value}
+        required
+    />
+);
+
+export default Input;
