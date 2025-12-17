c51bf9e26bd797e67099d43271a859004e980fff
Joshua Gato <joshuagato37@gmail.com>
2025-12-17 17:41:46 +0000

    Added redux store

---
+++ frontend/src/store/reducers/auth.js
@@ -0,0 +1,41 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const initialState = {
+    loading: false,
+    token: null,
+    userId: null,
+    error: "",
+    // error: [],
+    // isAuthenticated: false,
+    authRedirectPath: "/",
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.AUTH_START_LOADING:
+            return { ...state, loading: true };
+
+        case actionTypes.AUTH_STOP_LOADING:
+            return { ...state, loading: false };
+
+        case actionTypes.AUTH_SUCCESS:
+            // return { ...state, token: action.token, userId: action.userId, isAuthenticated: true };
+            return { ...state, token: action.token, userId: action.userId };
+
+        case actionTypes.AUTH_FAIL:
+            // return { ...state, token: null, userId: null, error: action.error, isAuthenticated: false };
+            return { ...state, token: null, userId: null, error: action.error };
+
+        case actionTypes.AUTH_LOGOUT:
+            // return { ...state, token: null, userId: null, isAuthenticated: false };
+            return { ...state, token: null, userId: null };
+
+        case actionTypes.SET_AUTH_REDIRECT_PATH:
+            return { ...state, authRedirectPath: action.path };
+
+        default:
+            return state;
+    }
+};
+
+export default reducer;
